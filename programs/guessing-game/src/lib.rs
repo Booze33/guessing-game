8d02fdb8f0129ff03fa1424c34ef5be5612dc91b
Booze33 <tebetisloh@gmail.com>
2024-07-11 02:00:48 +0200

    project setup

--- programs/guessing-game/src/lib.rs
+++ programs/guessing-game/src/lib.rs
@@ -10,7 +10,7 @@ use orao_solana_vrf::{
 pub mod misc;
 use self::misc::get_account_data;
 
-declare_id!("HMDRWmYvL2A9xVKZG8iA1ozxi4gMKiHQz9mFkURKrG4"); // ! UPDATE ME
+declare_id!("G6wY2q15hgJVdMxApMr516TVaAT24d9ps6Cgn1MSWfmW");
 
 #[program]
 pub mod guessing_game {
